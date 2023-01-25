5c1e7a25ee9578629421c5d8d211458a5c02eea5
HanawuZ <b6302245@gmail.com>
2023-01-25 21:46:38 +0700

    close #52 - add Class Schedule UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Subject_Management_Navbar from "./components/subject-management/Subject_Management_navbar";
+import Subject_Management_Footer from "./components/subject-management/Subject_Footer";
+import SubjectList from "./components/subject-management/Subject_List";
+import SubjectInfo from "./components/subject-management/Subject_Info";
+import CreateSubject from "./components/subject-management/Subject_Create";
+import UpdateSubject from "./components/subject-management/Subject_Update";
+import ScheduleList from "./components/class_and_exam_schedule/Schedule_List";
+import Exam_Schedule_Info from "./components/class_and_exam_schedule/Exam_Schedule_Info";
+import Class_Schedule_Info from "./components/class_and_exam_schedule/Class_Schedule_Info";
+import Class_Schedule_Create from "./components/class_and_exam_schedule/Class_Schedule_Create";
+import Exam_Schedule_Create from "./components/class_and_exam_schedule/Exam_Schedule_Create";
+import Class_Schedule_Update from "./components/class_and_exam_schedule/Class_Schedule_Update";
+import Exam_Schedule_Update from "./components/class_and_exam_schedule/Exam_Schedule_Update";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Subject_Management_Navbar />
+                <Routes>
+
+                    {/* //* List class schedules and exam schedules components */}
+                    <Route path="/" element={<ScheduleList />} />
+                    <Route path="/class_schedule" element={<ScheduleList/>} />
+                    <Route path="/exam_schedule" element={<ScheduleList/>} />
+                    
+                    {/* //* Class Schedule and Exam Schedule information components  */}
+                    <Route path="/class_schedule/:subject_id/:section" element={<Class_Schedule_Info/>} />
+                    <Route path="/exam_schedule/:subject_id/:exam_type" element={<Exam_Schedule_Info/>} />
+                    
+                    {/* //* Class Schedule Update component */}
+                    {/* //* Exam Schedule Update component */}
+
+                    {/* //* Class Schedule Create component */}
+                    <Route path="/class_schedule/create" element={<Class_Schedule_Create/>} />
+                    {/* //* Exam Schedule Create component */}
+                    <Route path="/exam_schedule/create" element={<Exam_Schedule_Create/>} />
+
+                    {/* //* Class Schedule Update component */}
+                    <Route path="/class_schedule/:subject_id/:section/update" element={<Class_Schedule_Update/>} />
+                    {/* //* Exam Schedule Update component */}
+                    <Route path="/exam_schedule/:subject_id/:exam_type/update" element={<Exam_Schedule_Update/>} />
+
+                </Routes>
+               
+
+
+                <Subject_Management_Footer />
+            </div>
+        </Router>
+    );
 }
-
-export default App;

682bdd94a0224d51b3190cc44b3b8efe894c871a
B6327552 <Aceportgasonepiece@gmail.com>
2023-02-06 14:35:40 +0700

    update - close #55

--- backend/validators/approval_validator_test.go
+++ backend/validators/approval_validator_test.go
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-package entity
-
-import (
-	"testing"
-
-	"github.com/B6025212/team05/entity"
-	"github.com/asaskevich/govalidator"
-	. "github.com/onsi/gomega"
-)
-
-// ตรวจสอบค่าว่างของชื่อแล้วต้องเจอ Error
-func TestApprovalReasonNotBlank(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	approval := entity.Approval{
-		Reason: "", // ผิด
-	}
-
-	// ตรวจสอบด้วย govalidator
-	ok, err := govalidator.ValidateStruct(approval)
-
-	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
-	g.Expect(ok).ToNot(BeTrue())
-
-	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
-	g.Expect(err).ToNot(BeNil())
-
-	// err.Error ต้องมี error message แสดงออกมา
-	g.Expect(err.Error()).To(Equal("Reason cannot be blank"))
-}
-
-func TestApprovalReasonNotSpecialCharacters(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	reason := entity.Approval{
-		Reason: "1234?$#@", // ผิด
-	}
-
-	// ตรวจสอบด้วย govalidator
-	ok, err := govalidator.ValidateStruct(reason)
-
-	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
-	g.Expect(ok).ToNot(BeTrue())
-
-	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
-	g.Expect(err).ToNot(BeNil())
-
-	// err.Error ต้องมี error message แสดงออกมา
-	g.Expect(err.Error()).To(Equal("Reason cannot be special characters or number"))
-}
-=======
 package entity
 
 import (
@@ -120,4 +68,3 @@ func TestMaxcharectorReason(t *testing.T) {
 	// err.Error() ต้องมี message แสดงออกมา
 	g.Expect(err.Error()).To(Equal("The reason cannot be more than 30 characters"))
 }
->>>>>>> dab0113 (add validation request - close #55)

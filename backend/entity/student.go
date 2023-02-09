0e681f3d37b27b92f482b5f42ce002d47bf1b71c
Archawat3333 <m_zoman3333@hotmail.com>
2023-02-09 15:19:34 +0700

    implement validation student_id - close #97

--- backend/entity/student.go
+++ backend/entity/student.go
@@ -2,6 +2,8 @@ package entity
 
 import (
 	"time"
+
+	validator "github.com/asaskevich/govalidator"
 )
 
 // Dormitory
@@ -13,12 +15,12 @@ type Dormitory struct {
 }
 
 type Student struct {
-	Student_ID string `gorm:"primaryKey"`
+	Student_ID string `gorm:"primaryKey" valid:"studentchecklenght~Student ID Should be 8 characters,required~Student ID cannot be null"`
 
-	Student_Name string
+	Student_Name string `valid:"studentnamechecklanguage~Student Name cannot be English Language,required~Student Name cannot be null,maxstringlength(40)~Student Name can not greater than 40 character"`
 
-	Student_Password string
-	Datetime         time.Time
+	Student_Password string    `valid:"required~Student Password cannot be null,studentpasswordchecklanguage~Student Password cannot be Thai Language"`
+	Datetime         time.Time `valid:"required~Datetime cannot be null,datetimecheckfuture~Datetime cannot be future"`
 
 	Admin_ID *string `valid:"-"`
 	Admin    Admin   `gorm:"references:Admin_ID" valid:"-"`
@@ -30,4 +32,17 @@ type Student struct {
 	Dormitory    Dormitory `gorm:"references:Dormitory_ID" valid:"-"`
 
 	Enrolls []Enroll `gorm:"foreignKey:Student_ID" valid:"-"`
+}
+
+func SetStudentIDValidation() {
+	validator.CustomTypeTagMap.Set("studentchecklenght", validator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
+
+		str := i.(string)
+		if len(str) == 8 {
+			return true
+		} else {
+			return false
+		}
+	}))
+
 }
